11f396a95b0b323afcb0ec8baaf0f2ca4815ea38
NastiaKoltyka <Nusia.k5669@gmail.com>
2021-09-20 00:03:54 +0300

    add button down

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let picture = document.querySelectorAll('.picture');
 let text = document.querySelectorAll('.text');
 const main = document.querySelector('.container');
 let items = document.querySelectorAll('.items').length;
+let button = document.querySelector('.btn');
 let page = 0;
 let active = true;
 
@@ -10,20 +11,8 @@ const scroll = (event) => {
         return false
     }
     if (event.deltaY > 0 && page < items - 1) {
-        page = page + 1;
-        console.log(page % 2);
-        main.style.top = `${-window.innerHeight*page}px`;
         active = false;
-        setTimeout(() => {
-            if (page % 2) {
-                picture[page - 1].style.opacity = `1`;
-                text[page - 1].style.cssText = 'width:66%; opacity:1; right:0;';
-            } else {
-                picture[page - 1].style.opacity = `1`;
-                text[page - 1].style.cssText = 'width:66%; opacity:1; left:0;';
-            }
-
-        }, 1000);
+        changePage();
         setTimeout(() => {
             active = true;
         }, 2000);
@@ -36,4 +25,24 @@ const scroll = (event) => {
         }, 2000);
     }
 };
+const scrollDown = () => {
+    changePage();
+};
+const changePage = () => {
+    if (page < items - 1) {
+        page = page + 1;
+        main.style.top = `${-window.innerHeight*page}px`;
+        setTimeout(() => {
+            if (page % 2) {
+                picture[page - 1].style.opacity = `1`;
+                text[page - 1].style.cssText = 'width:66%; opacity:1; right:0;';
+            } else {
+                picture[page - 1].style.opacity = `1`;
+                text[page - 1].style.cssText = 'width:66%; opacity:1; left:0;';
+            }
+        }, 1000);
+    }
+
+}
 main.addEventListener("wheel", scroll);
+button.addEventListener("click", scrollDown);
