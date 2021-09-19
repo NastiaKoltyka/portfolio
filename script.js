c028f6461b9b7f52a5a0cae08c388a8aa9e0b0aa
NastiaKoltyka <Nusia.k5669@gmail.com>
2021-09-19 14:30:43 +0300

    add scroll an video

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-let onePicture = document.querySelector('.project-one-picture');
-let twoPicture = document.querySelector('.project-two-picture');
-let oneTextContainer=document.querySelector('.one-text-container');
-let twoTextContainer=document.querySelector('.two-text-container');
+let picture = document.querySelectorAll('.picture');
+let text = document.querySelectorAll('.text');
 const main = document.querySelector('.container');
-let height=window.innerHeight;
-const scrollEvent = () => {
-    console.log('height: ', height);
-    console.log('scroll: ', main.scrollTop);
-    if (main.scrollTop >= 100 && main.scrollTop <= height) {
-        onePicture.style.opacity = `1`;
-        oneTextContainer.style.cssText = 'width:66%; opacity:1; right:0;';
-    }
-    else if (main.scrollTop > height+100){
-        twoPicture.style.opacity = `1`;
-        twoTextContainer.style.cssText = 'width:66%; opacity:1; left:0;';
-    }
-    else {
-        onePicture.style.opacity = `0.2`;
-    }
+let items = document.querySelectorAll('.items').length;
+let page = 0;
+let active = true;
 
-}
-main.addEventListener('scroll', scrollEvent);
+const scroll = (event) => {
+    if (!active) {
+        return false
+    }
+    if (event.deltaY > 0 && page < items - 1) {
+        page = page + 1;
+        console.log(page % 2);
+        main.style.top = `${-window.innerHeight*page}px`;
+        active = false;
+        setTimeout(() => {
+            if (page % 2) {
+                picture[page - 1].style.opacity = `1`;
+                text[page - 1].style.cssText = 'width:66%; opacity:1; right:0;';
+            } else {
+                picture[page - 1].style.opacity = `1`;
+                text[page - 1].style.cssText = 'width:66%; opacity:1; left:0;';
+            }
+
+        }, 1000);
+        setTimeout(() => {
+            active = true;
+        }, 2000);
+    } else if (event.deltaY < 0 && page > 0) {
+        page = page - 1;
+        main.style.top = `${-window.innerHeight*page}px`;
+        active = false;
+        setTimeout(() => {
+            active = true;
+        }, 2000);
+    }
+};
+main.addEventListener("wheel", scroll);
